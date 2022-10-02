4bfb700505eace6a9ef5c470bd4fb10924ee9a54
a-isaiahharvey <a-isaiahharvey@users.noreply.github.com>
2022-10-02 21:02:28 +0000

    deploy: a519350c33e1952e63817348689657b4124d3dbf

--- frontend-9f168152a5cb66ec.js
+++ frontend-9f168152a5cb66ec.js
@@ -799,7 +799,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('frontend-448749562af916eb_bg.wasm', import.meta.url);
+        input = new URL('frontend-9f168152a5cb66ec_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
